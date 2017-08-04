6be495bde08102f72ca132371bfbb6dd03500108
Surfacescan <sullivan.bart@gmail.com>
2017-08-04 13:27:22 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var TelegramBot = require('node-telegram-bot-api'),
+    // Be sure to replace YOUR_BOT_TOKEN with your actual bot tokenon this line.
+    telegram = new TelegramBot("430756777:AAE1F46XZ6ICPQfts0nWkxccqKEDliqfr4o", { polling: true });
+var request = require('request');
+
+telegram.on("text", (message) => {
+  if(message.text.toLowerCase().indexOf("/ngrok") === 0){
+
+
+  request('http://127.0.0.1:4042/api/tunnels/', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body);
+
+    var result = JSON.parse(body);
+    var tunnel_text = "";
+    for(var t in result.tunnels) {
+         console.log(result.tunnels[t].name +':'+ result.tunnels[t].public_url);
+    }
+    for(var t in result.tunnels) {
+         tunnel_text += '🚆' + result.tunnels[t].name +' : '+ result.tunnels[t].public_url + "\n";
+    }
+
+ 
+    if( error && error.code =="ECONNREFUSED"){
+       telegram.sendMessage(message.chat.id, "error: `" + error.code + "` - ngrok running?",
+           {
+             parse_mode: "markdown" 
+           }
+           
+           );
+    }
+    else{
+       telegram.sendMessage(message.chat.id, "*Ngrok Tunnels* \n" + tunnel_text,
+           {
+              parse_mode: "markdown"
+           }
+           
+           );
+    }
+
+    });
+    
+  }
+
+});
